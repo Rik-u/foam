413df1cb5ffb26ec5d70a16e6023517ec1c9bfda
Riku Voraniphit <voraniphit.k@ku.th>
2025-10-08 15:07:57 +0700

    Search bar

--- src/app/admin/components/ItemList.tsx
+++ src/app/admin/components/ItemList.tsx
@@ -3,7 +3,7 @@ import { Item } from "@/app/admin/types"
 
 export default function ItemList({ list } : { list : Item[] }) {
     return (
-        <div className="grid grid-cols-5 gap-3">
+        <div className="grid grid-cols-5 gap-3 m-4">
             {list.map(item => (
                 <div key={item.id} className="outline-2 rounded-xl p-3">
                     <Image 
--- src/app/admin/components/TopBar.tsx
+++ src/app/admin/components/TopBar.tsx
@@ -2,7 +2,7 @@ import SearchBar from "./SearchBar";
 
 export default function TopBar() {
     return (
-        <div className="h-16 bg-blue-400">
+        <div className="flex items-center justify-center h-14 bg-blue-400">
             <SearchBar />
         </div>
     );
