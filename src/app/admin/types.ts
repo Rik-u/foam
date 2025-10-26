7beae55cb59a74512e2f462b2532526e1bb2e09e
Riku Voraniphit <voraniphit.k@ku.th>
2025-10-26 18:48:13 +0700

    Delete unused import

--- src/app/admin/types.ts
+++ src/app/admin/types.ts
@@ -1,13 +1,7 @@
-import type { ItemType } from "@/types/Item";
-
 export interface ItemProps {
     id: number;
     title: string;
     onClickDelete: (id: number) => void;
-}
-
-export interface ItemGridProps {
-    items: ItemType[]
 }
 
 export interface ItemAddButtonProps {
